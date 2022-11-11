b0f54d766c4d18bfe9bb72f73afb3cc241e889cc
Matthew Greenberg <mgreenbe@ucalgary.ca>
2022-11-11 14:49:54 -0500

    lit-element -> lit

--- src/my-element.ts
+++ src/my-element.ts
@@ -1,55 +1,24 @@
-import { LitElement, html, customElement, property, css } from 'lit-element'
+import { LitElement, html, customElement, state } from "lit-element";
 
-/**
- * An example element.
- *
- * @slot - This element has a slot
- * @csspart button - The button
- */
-@customElement('my-element')
+@customElement("my-element")
 export class MyElement extends LitElement {
-  static styles = css`
-    :host {
-      display: block;
-      border: solid 1px gray;
-      padding: 16px;
-      max-width: 800px;
-    }
-  `
-
-  /**
-   * The name to say "Hello" to.
-   */
-  @property()
-  name = 'World'
-
-  /**
-   * The number of times the button has been clicked.
-   */
-  @property({ type: Number })
-  count = 0
+  @state()
+  private _count = 0;
 
   render() {
     return html`
-      <h1>Hello, ${this.name}!</h1>
-      <button @click=${this._onClick} part="button">
-        Click Count: ${this.count}
-      </button>
+      <button @click=${this._onClick}>Click Count: ${this._count}</button>
       <slot></slot>
-    `
+    `;
   }
 
   private _onClick() {
-    this.count++
-  }
-
-  foo(): string {
-    return 'foo'
+    this._count++;
   }
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'my-element': MyElement
+    "my-element": MyElement;
   }
 }
